cd2f9d17874e7d4f1f51335c0eb382779274aed5
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-07-08 17:51:35 +0100

    Scripts for adding new sites

--- docker.ps1
+++ docker.ps1
@@ -1,7 +1,12 @@
 param(
   [string]
-  $command
+  $command,
+
+  [parameter(ValueFromRemainingArguments = $true)]
+  [string[]]$Parameters
 )
+
+Import-Module PSYaml
 
 function Invoke-DockerUp {
   Import-EnvFile {
@@ -81,14 +86,74 @@ function Import-EnvFile([scriptblock] $block) {
   }
 }
 
+function Add-Site {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $name
+  )
+
+  if (!$name) {
+    throw "Name is required."
+  }
+
+  if (! (Test-Path "$PSScriptRoot\$name")) {
+    mkdir "$PSScriptRoot\$name";
+  }
+
+  if (! (Test-Path "$PSScriptRoot\$name\docker-compose.yml")) {
+    Copy-Item "$PSScriptRoot\.docker\docker-compose-template.yml" "$PSScriptRoot\$name\docker-compose.yml"
+  }
+
+  if (! (Test-Path "$PSScriptRoot\$name\.env")) {
+    $env_template = Get-Content "$PSScriptRoot\.docker\template.env" -Raw
+    $env_template = $env_template.Replace('${SITE_NAME}', $name).Replace('${BASE_URL}', "$name.localhost");
+    $env_template | Out-File "$PSScriptRoot\$name\.env" -Encoding utf8
+  }
+
+  # Read the root file and update the site networks
+  $yaml = Get-Content "$PSScriptRoot\docker-compose.yml" -Raw
+  $config = ConvertFrom-Yaml $yaml
+  
+  # Add a network that matches the new container
+  $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
+  # Add network to traefik proxy
+  $config.services.traefik.networks += $name
+  $config.services.db.networks += $name
+
+  ConvertTo-Yaml $config | Out-File "$PSScriptRoot\docker-compose.yml" -Encoding utf8
+}
+
+function Rebuild-Networks {
+  $directories = Get-ChildItem -Directory "$PSScriptRoot" | Where-Object { !$_.Name.StartsWith(".") }
+  $yaml = Get-Content "$PSScriptRoot\docker-compose.yml" -Raw
+  $config = ConvertFrom-Yaml $yaml
+
+  foreach($dir in $directories) {
+    $name = $dir.Name
+    if (!$config.networks[$name]) {
+      $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
+      $config.services.traefik.networks += $name
+      $config.services.db.networks += $name
+    }
+  }
+
+  # Restart the container before making the changes
+  & docker-compose down
+  ConvertTo-Yaml $config | Out-File "$PSScriptRoot\docker-compose.yml" -Encoding utf8
+  & docker-compose up -d
+}
+
 switch($command) {
-  'up'    { Invoke-DockerUp $args }
-  'stop'  { Invoke-DockerStop $args }
-  'rm'    { Invoke-DockerRm $args }
-  'ls'    { Invoke-DockerLs $args }
-  'sh'    { Invoke-DockerSh $args }
-  'logs'  { Invoke-DockerLogs $args }
+  'up'    { Invoke-DockerUp @Parameters }
+  'stop'  { Invoke-DockerStop @Parameters }
+  'rm'    { Invoke-DockerRm @Parameters }
+  'ls'    { Invoke-DockerLs @Parameters }
+  'sh'    { Invoke-DockerSh @Parameters }
+  'logs'  { Invoke-DockerLogs @Parameters }
+  'add-site' { Add-Site @Parameters }
+  'rebuild-networks' { Rebuild-Networks @Parameters }
   default {
-    write-host "The following commands are available: up, stop, rm, ls, sh, logs"
+    write-host "The following commands are available: up, stop, rm, ls, sh, logs, rebuild-networks, add-site"
   }
 }
