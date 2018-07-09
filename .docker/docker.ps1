e07e4980e18fdc0a6827609458a49872c565ed9a
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-07-09 15:01:21 +0100

    Remove some commands for now as all the powershell yaml parsers are crap

--- .docker/docker.ps1
+++ .docker/docker.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'stop'
 
-Import-Module PSYaml
+# Import-Module PSYaml
 
 $options = @(
   [System.Management.Automation.Host.ChoiceDescription]::new('&No', 'No')
@@ -94,85 +94,85 @@ function Import-EnvFile([scriptblock] $block) {
   }
 }
 
-function Add-Site {
-  param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $name
-  )
+# function Add-Site {
+#   param(
+#     [Parameter(Mandatory=$true)]
+#     [string]
+#     $name
+#   )
 
-  if (!$name) {
-    throw "Name is required."
-  }
+#   if (!$name) {
+#     throw "Name is required."
+#   }
 
-  if (! (Test-Path "$PSScriptRoot\..\$name")) {
-    mkdir "$PSScriptRoot\..\$name";
-  }
+#   if (! (Test-Path "$PSScriptRoot\..\$name")) {
+#     mkdir "$PSScriptRoot\..\$name";
+#   }
 
-  if (! (Test-Path "$PSScriptRoot\..\$name\docker-compose.yml")) {
-    Copy-Item "$PSScriptRoot\docker-compose-template.yml" "$PSScriptRoot\..\$name\docker-compose.yml"
-  }
+#   if (! (Test-Path "$PSScriptRoot\..\$name\docker-compose.yml")) {
+#     Copy-Item "$PSScriptRoot\docker-compose-template.yml" "$PSScriptRoot\..\$name\docker-compose.yml"
+#   }
 
-  if (! (Test-Path "$PSScriptRoot\..\$name\docker.ps1")) {
-    Copy-Item "$PSScriptRoot\docker.template.ps1" "$PSScriptRoot\..\$name\docker.ps1"
-  }
+#   if (! (Test-Path "$PSScriptRoot\..\$name\docker.ps1")) {
+#     Copy-Item "$PSScriptRoot\docker.template.ps1" "$PSScriptRoot\..\$name\docker.ps1"
+#   }
 
-  if (! (Test-Path "$PSScriptRoot\..\$name\.env")) {
-    $env_template = Get-Content "$PSScriptRoot\template.env" -Raw
-    $env_template = $env_template.Replace('${SITE_NAME}', $name).Replace('${BASE_URL}', "$name.localhost");
-    $env_template | Out-File "$PSScriptRoot\..\$name\.env" -Encoding utf8
-  }
+#   if (! (Test-Path "$PSScriptRoot\..\$name\.env")) {
+#     $env_template = Get-Content "$PSScriptRoot\template.env" -Raw
+#     $env_template = $env_template.Replace('${SITE_NAME}', $name).Replace('${BASE_URL}', "$name.localhost");
+#     $env_template | Out-File "$PSScriptRoot\..\$name\.env" -Encoding utf8
+#   }
 
-  # Read the root file and update the site networks
-  $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
-  $config = ConvertFrom-Yaml $yaml
+#   # Read the root file and update the site networks
+#   $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
+#   $config = ConvertFrom-Yaml $yaml
 
-  # Add a network that matches the new container
-  $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
-  # Add network to traefik proxy
-  $config.services.traefik.networks += $name
-  $config.services.db.networks += $name
-  $config.services.mailhog.networks += $name
+#   # Add a network that matches the new container
+#   $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
+#   # Add network to traefik proxy
+#   $config.services.traefik.networks += $name
+#   $config.services.db.networks += $name
+#   $config.services.mailhog.networks += $name
 
-  $yaml = ConvertToYaml $config
-  $yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
-}
-function Rebuild-Networks {
-  $directories = Get-ChildItem -Directory "$PSScriptRoot\.." | Where-Object { !$_.Name.StartsWith(".") }
-  $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
-  $config = ConvertFrom-Yaml $yaml
+#   $yaml = ConvertToYaml $config
+#   $yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
+# }
+# function Rebuild-Networks {
+#   $directories = Get-ChildItem -Directory "$PSScriptRoot\.." | Where-Object { !$_.Name.StartsWith(".") }
+#   $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
+#   $config = ConvertFrom-Yaml $yaml
 
-  foreach($dir in $directories) {
-    $name = $dir.Name
-    if (!$config.networks[$name]) {
-      $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
-      $config.services.traefik.networks += $name
-      $config.services.db.networks += $name
-      $config.services.mailhog.networks += $name
-    }
-  }
+#   foreach($dir in $directories) {
+#     $name = $dir.Name
+#     if (!$config.networks[$name]) {
+#       $config.networks[$name] = @{ 'external' = @{ 'name' = "${name}_default" } }
+#       $config.services.traefik.networks += $name
+#       $config.services.db.networks += $name
+#       $config.services.mailhog.networks += $name
+#     }
+#   }
 
-  # Restart the container before making the changes
-  & docker-compose down
-  $yaml = ConvertToYaml $config
-  $yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
-  & docker-compose up -d
-}
+#   # Restart the container before making the changes
+#   & docker-compose down
+#   $yaml = ConvertToYaml $config
+#   $yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
+#   & docker-compose up -d
+# }
 
-function ConvertToYaml ($obj) {
-  # The default PSYaml serializer does not serialize well.
-  $yaml = ConvertTo-Yaml $obj
-  #return $yaml
+# function ConvertToYaml ($obj) {
+#   # The default PSYaml serializer does not serialize well.
+#   $yaml = ConvertTo-Yaml $obj
+#   #return $yaml
 
-  # Get rid of the \r characters
-  # Skip the unnecessary "---" line
-  # Fix the indent and remove the space from the end of each line
-  # Add extra indent for list lines (as they're not indented enough)
-  $yaml.Split("`r", [System.StringSplitOptions]::RemoveEmptyEntries) `
-    | Select-Object -Skip 1 `
-    | ForEach-Object { $_.Substring(3).TrimEnd()  }
-    #| ForEach-Object { if ($_ -match  "^\W.+-") { "  $_" } else { $_ } }
-}
+#   # Get rid of the \r characters
+#   # Skip the unnecessary "---" line
+#   # Fix the indent and remove the space from the end of each line
+#   # Add extra indent for list lines (as they're not indented enough)
+#   $yaml.Split("`r", [System.StringSplitOptions]::RemoveEmptyEntries) `
+#     | Select-Object -Skip 1 `
+#     | ForEach-Object { $_.Substring(3).TrimEnd()  }
+#     #| ForEach-Object { if ($_ -match  "^\W.+-") { "  $_" } else { $_ } }
+# }
 
 function Invoke-Docker {
   param(
@@ -191,15 +191,16 @@ function Invoke-Docker {
     'ls'    { Invoke-DockerLs @Parameters }
     'sh'    { Invoke-DockerSh @Parameters }
     'logs'  { Invoke-DockerLogs @Parameters }
-    'add-site' { Add-Site @Parameters }
-    'rebuild-networks' { Rebuild-Networks @Parameters }
-    'test' {
-      $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
-  $config = ConvertFrom-Yaml $yaml
-$yaml = ConvertToYaml $config
-$yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
+   # 'add-site' { Add-Site @Parameters }
+   # 'rebuild-networks' { Rebuild-Networks @Parameters }
+   # 'test' {
+   #   $yaml = Get-Content "$PSScriptRoot\..\docker-compose.yml" -Raw
+  #$config = ConvertFrom-Yaml $yaml
+#$yaml = ConvertToYaml $config
 
-    }
+#$yaml | Out-File "$PSScriptRoot\..\docker-compose.yml" -Encoding utf8
+
+ #   }
     default {
       write-host "The following commands are available: up, stop, rm, ls, sh, logs, rebuild-networks, add-site"
     }
